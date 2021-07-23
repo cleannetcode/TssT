90c59240987e4b3dde2f47694e7af4a7715a5de9
Mikhail Slipin <mikhail.slipin@waveaccess.ru>
2021-07-23 23:26:00 +0300

    feature: task-6 / добавил интерфейс и класс для доступа к данным о топиках

--- TssT.DataAccess/Configurations/TopicConfiguration.cs
+++ TssT.DataAccess/Configurations/TopicConfiguration.cs
@@ -4,9 +4,15 @@ using TssT.DataAccess.Entities;
 
 namespace TssT.DataAccess.Configurations
 {
+<<<<<<< HEAD:TssT.DataAccess/Configurations/TopicGroupConfiguration.cs
+    public class TopicGroupConfiguration:IEntityTypeConfiguration<TopicGroup>
+    {
+        public void Configure(EntityTypeBuilder<TopicGroup> builder)
+=======
     public class TopicConfiguration: IEntityTypeConfiguration<Topic>
     {
         public void Configure(EntityTypeBuilder<Topic> builder)
+>>>>>>> 6117b47 (feature: task-6 / добавил интерфейс и класс для доступа к данным о топиках):TssT.DataAccess/Configurations/TopicConfiguration.cs
         {
             builder.HasKey(obj => obj.Id);
         }
