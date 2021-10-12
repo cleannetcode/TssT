26f2ee254183edef29d02c17f8457243b2117777
Mikhail Coder <coder1coder@gmail.com>
2021-10-12 09:23:08 +0300

    test repo with interface

---
+++ TssT.DataAccess/Repositories/Test/ITestRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using TssT.DataAccess.Entities;
+
+namespace TssT.DataAccess.Repositories.Test
+{
+    public interface ITestRepository
+    {
+        public Task<int> InsertAsync(Entities.Test entity);
+        public Task DeleteAsync(int id);
+    }
+}
---
+++ TssT.DataAccess/Repositories/Test/TestRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TssT.DataAccess.Entities;
+
+namespace TssT.DataAccess.Repositories.Test
+{
+    public class TestRepository: ITestRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public TestRepository(
+            ApplicationDbContext dbContext
+        )
+        {
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+
+        public async Task<int> InsertAsync(Entities.Test entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+
+            await _dbContext.Tests.AddAsync(entity);
+
+            return entity.Id;
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            if (id <= default(int))
+                throw new ArgumentOutOfRangeException(nameof(id), $"Id must be grater then {default(int)}");
+
+            var entity = await _dbContext.Tests.FirstOrDefaultAsync(x => x.Id == id);
+            
+            _dbContext.Tests.Remove(entity);
+            
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
