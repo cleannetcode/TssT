a0f57a7bba6e99548a96903cdbe402b0c02e9008
Mikhail Coder <coder1coder@gmail.com>
2021-10-12 09:23:19 +0300

    test service with interface

---
+++ TssT.Businesslogic/Services/Test/ITestService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace TssT.Businesslogic.Services.Test
+{
+    public interface ITestService
+    {
+        public Task<int> CreateAsync(Core.Models.Test dto);
+        public Task DeleteAsync(int id);
+    }
+}
---
+++ TssT.Businesslogic/Services/Test/TestService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using AutoMapper;
+using TssT.DataAccess.Repositories.Test;
+
+namespace TssT.Businesslogic.Services.Test
+{
+    public class TestService: ITestService
+    {
+        private readonly IMapper _mapper;
+        private readonly ITestRepository _testRepository;
+        
+        public TestService(
+            IMapper mapper,
+            ITestRepository testRepository
+            )
+        {
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _testRepository = testRepository ?? throw new ArgumentNullException(nameof(testRepository));
+        }
+        
+        public async Task<int> CreateAsync(Core.Models.Test dto)
+        {
+            var entity = _mapper.Map<DataAccess.Entities.Test>(dto);
+            return await _testRepository.InsertAsync(entity);
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            await _testRepository.DeleteAsync(id);
+        }
+    }
+}
