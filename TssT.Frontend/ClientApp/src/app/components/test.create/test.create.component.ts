fbddcd9112bcc37f07af11baf1c210ec3ea82958
Mikhail Coder <coder1coder@gmail.com>
2021-10-15 16:57:17 +0300

    компонент формы создания теста

---
+++ TssT.Frontend/ClientApp/src/app/components/test.create/test.create.component.ts
@@ -0,0 +1,93 @@
+﻿import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import {Test} from "../../models/Test";
+import {Topic} from "../../models/Topic";
+import {MatTableDataSource} from "@angular/material/table";
+import {TestService} from "../../services/test.service";
+import {MatPaginator} from '@angular/material/paginator';
+import {Router} from "@angular/router";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {FormBuilder} from "@angular/forms";
+
+@Component({
+  selector: 'test-create-form',
+  templateUrl: './test.create.component.html',
+  styleUrls: ['./test.create.component.scss'],
+})
+
+export class TestCreateComponent implements AfterViewInit {
+
+  displayedColumns: string[] = ['position', 'name'];
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  @ViewChild('testName') newTestNameInput:ElementRef;
+  @ViewChild('topicName') newTopicNameInput:ElementRef;
+
+
+  public test: Test
+
+  public testForm = this.formBuilder.group({
+    name: [''],
+    description: ['']
+  });
+
+  public topicsDataSource: MatTableDataSource<Topic>
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private testService: TestService,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) {
+    this.test = new Test();
+    this.topicsDataSource = new MatTableDataSource<Topic>(this.test.topics);
+  }
+
+  ngAfterViewInit(): void {
+    this.topicsDataSource.paginator = this.paginator;
+
+    this.newTestNameInput.nativeElement.focus();
+  }
+
+  validateTopicNameInput() : boolean{
+    let value = this.newTopicNameInput.nativeElement.value;
+    return value.length >= 3 && this.test.topics.filter(x=>x.name == value).length == 0;
+  }
+
+  addTopic(topicName: string){
+
+    if (!this.validateTopicNameInput())
+      return;
+
+    let topic = new Topic();
+    topic.name = topicName;
+    this.test.topics.push(topic);
+
+    this.newTopicNameInput.nativeElement.value = "";
+
+    this.refreshTopicsTable();
+  }
+
+  refreshTopicsTable(){
+    this.topicsDataSource.data = this.test.topics;
+  }
+
+  save(){
+
+    this.testService
+      .saveTest(this.testForm.value)
+      .subscribe({
+        next: value => {
+          if (value.id > 0){
+            this.router.navigate(['/lk']);
+          }
+        },
+        error: err => {
+          this.snackBar.open(err.message, "ok", { duration: 5 * 1000 });
+        }
+      });
+  }
+
+}
